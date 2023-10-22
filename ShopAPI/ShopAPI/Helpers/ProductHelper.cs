e332cf9498bb15602ec98c732f863cf3aee45f74
evan-scales <evan.m.scales@gmail.com>
2023-10-21 23:31:17 -0400

    Simplified GetRequiredPropertiesForCategory

--- ShopAPI/ShopAPI/Helpers/ProductHelper.cs
+++ ShopAPI/ShopAPI/Helpers/ProductHelper.cs
@@ -1,9 +1,11 @@
 ﻿using ShopAPI.Models;
+using System.Reflection;
 
 namespace ShopAPI.Helpers
 {
     public static class ProductHelper
     {
+        // L
         public static string? ValidateProductDetails(Category category, Dictionary<string, string> details)
         {
             var requiredProperties = GetRequiredPropertiesForCategory(category);
@@ -36,15 +38,29 @@ namespace ShopAPI.Helpers
 
         private static IEnumerable<string>? GetRequiredPropertiesForCategory(Category category)
         {
+            IEnumerable<string> requiredProperties;
+            var ignoreProperties = typeof(Product).GetProperties().Select(p => p.Name);
+            
+
             switch (category)
             {
                 case Category.Cpu:
-                    return new[] { "socket", "cores", "series", "integratedGraphics" };
+
+                    var cpuProperties = typeof(Cpu).GetProperties();
+                    requiredProperties = cpuProperties.Select(p => p.Name);
+                    break;
+
                 case Category.Case:
-                    return new[] { "color", "formFactor", "sidePanel", "powerSupply" };
+
+                    var caseProperties = typeof(Case).GetProperties();
+                    requiredProperties = caseProperties.Select(p => p.Name);
+                    break;
+
                 default:
                     return null;
             }
+
+            return requiredProperties.Except(ignoreProperties, StringComparer.OrdinalIgnoreCase);
         }
     }
 }
