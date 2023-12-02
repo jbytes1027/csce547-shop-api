4da6ae81090319a9788772abd50253c0ab90a872
James Pretorius <jamesmpretorius@gmail.com>
2023-12-02 16:21:08 -0500

    Remove Cart from CartItem

--- ShopAPI/ShopAPI/Models/CartItem.cs
+++ ShopAPI/ShopAPI/Models/CartItem.cs
@@ -1,11 +1,13 @@
-﻿namespace ShopAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopAPI.Models
 {
     public class CartItem
     {
         public Product Product { get; set; } = null!;
         public int ProductId { get; set; }
+        [ForeignKey(nameof(Cart))]
         public int CartId { get; set; }
-        public Cart Cart { get; set; } = null!;
         public int Quantity { get; set; } = 1;
     }
 }
